a25c5737ee58e4465788e25d068d4816656fe294
vukani <gcabashevukani3@gmail.com>
2022-10-04 13:56:52 -1200

    updated ships
    removed element stopping the ui from displaying correctly

--- src/Ships.js
+++ src/Ships.js
@@ -51,53 +51,51 @@ function Ships(){
           </form>
         </div>
       </div>
-    <div className="">
+    <div className="flex flex-row">
         <div className="grid grid-cols-4 gap-2">
         
-          { foundShips && foundShips.length > 0 ? (
-            foundShips.map((ship, index) => (
-              <div key={index} >
-                    <div className="mx-2 grid max-w-sm rounded ">
+          { foundShips && foundShips.length > 0 ? ( foundShips.map((ship, index) => (
+              <div key={index} >1
+                  <div className="mx-2 grid max-w-sm rounded ">
                     <img src='https://i.imgur.com/Wr1slIc.png'  className="w-full max-w-sm rounded-lg overflow-hidden shadow-xl" alt='Ship - spaceX'/>
-                        {/* will add onerror that works correctly after all other things work */}
-                        {/* <img  onError={({ currentTarget }) => { currentTarget.src="https://farm5.staticflickr.com/4599/38583829295_581f34dd84_b.jpg";}} src={ship.image} className="img rounded-lg shadow-xl" alt='Ship - spaceX'style={{height: "10rem"}}  /> */}
-                        <div className="px-2 my-4">
-                            <h3 className="uppercase font-bold text-xl mb-2">{ship.name} </h3>
-                        </div>
-                        <div className="px-2 pt-2 pb-2">
-                            <p className="uppercase font-bold block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">mass: { ship.mass_lbs } lbs</p>
-                            <p className="uppercase font-bold block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">year built: { ship.year_built } </p>
-                        </div>
-                        <button className="h-12 uppercase mb-4 justify-self-center bg-black hover:bg-blue-700 text-white font-bold py-2 rounded w-11/12">  learn more </button>
+                    {/* will add onerror that works correctly after all other things work */}
+                    {/* <img  onError={({ currentTarget }) => { currentTarget.src="https://farm5.staticflickr.com/4599/38583829295_581f34dd84_b.jpg";}} src={ship.image} className="img rounded-lg shadow-xl" alt='Ship - spaceX'style={{height: "10rem"}}  /> */}
+                    <div className="px-2 my-4">
+                        <h3 className="uppercase font-bold text-xl mb-2">{ship.name} </h3>
                     </div>
+                    <div className="px-2 pt-2 pb-2">
+                        <p className="uppercase font-bold block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">mass: { ship.mass_lbs } lbs</p>
+                        <p className="uppercase font-bold block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">year built: { ship.year_built } </p>
+                    </div>
+                    <button className="h-12 uppercase mb-4 justify-self-center bg-black hover:bg-blue-700 text-white font-bold py-2 rounded w-11/12">  learn more </button>
+                  </div>
                 </div>
               
             ))
           ) : (
-            <div className='flex'>
-            {
-              data?.data?.map((ship, index) => { 
-              
-                  return (
-                  <div key={index} >
-                      <div className="mx-2 grid max-w-sm rounded ">
-                      <img src='https://i.imgur.com/Wr1slIc.png'  className="w-full max-w-sm rounded-lg overflow-hidden shadow-xl" alt='Ship - spaceX'/>
-                          {/* will add onerror that works correctly after all other things work */}
-                          {/* <img  onError={({ currentTarget }) => { currentTarget.src="https://farm5.staticflickr.com/4599/38583829295_581f34dd84_b.jpg";}} src={ship.image} className="img rounded-lg shadow-xl" alt='Ship - spaceX'style={{height: "10rem"}}  /> */}
-                          <div className="px-2 my-4">
-                              <h3 className="uppercase font-bold text-xl mb-2">{ship.name} {index}</h3>
-                          </div>
-                          <div className="px-2 pt-2 pb-2">
-                              <p className="uppercase font-bold block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">mass: { ship.mass_lbs } lbs</p>
-                              <p className="uppercase font-bold block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">year built: { ship.year_built } </p>
-                          </div>
-                          <button className="h-12 uppercase mb-4 justify-self-center bg-black hover:bg-blue-700 text-white font-bold py-2 rounded w-11/12">  learn more </button>
-                      </div>
-                  </div>      
-                  )
-              }) 
-              }    
-            </div>
+              <>
+              {
+                data?.data?.map((ship, index) => { 
+                    return (
+                    <div key={index} >2
+                        <div className="mx-2 grid max-w-sm rounded">
+                        <img src='https://i.imgur.com/Wr1slIc.png'  className="w-full max-w-sm rounded-lg overflow-hidden shadow-xl" alt='Ship - spaceX'/>
+                            {/* will add onerror that works correctly after all other things work */}
+                            {/* <img  onError={({ currentTarget }) => { currentTarget.src="https://farm5.staticflickr.com/4599/38583829295_581f34dd84_b.jpg";}} src={ship.image} className="img rounded-lg shadow-xl" alt='Ship - spaceX'style={{height: "10rem"}}  /> */}
+                            <div className="px-2 my-4">
+                                <h3 className="uppercase font-bold text-xl mb-2">{ship.name} {index}</h3>
+                            </div>
+                            <div className="px-2 pt-2 pb-2">
+                                <p className="uppercase font-bold block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">mass: { ship.mass_lbs } lbs</p>
+                                <p className="uppercase font-bold block bg-gray-100 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">year built: { ship.year_built } </p>
+                            </div>
+                            <button className="h-12 uppercase mb-4 justify-self-center bg-black hover:bg-blue-700 text-white font-bold py-2 rounded w-11/12">  learn more </button>
+                        </div>
+                    </div>      
+                    )
+                  }) 
+                }    
+              </>
           )}
         </div>
       </div>

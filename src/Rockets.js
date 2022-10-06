d6fc85beb2b8129901bc61cef1f631038246dcee
vukani <gcabashevukani3@gmail.com>
2022-10-06 08:59:18 -1200

    history component align and search
    adding search and pagination to our history component and then also some alignments

--- src/Rockets.js
+++ src/Rockets.js
@@ -8,7 +8,6 @@ function getKg(mass){ return (mass*9.80).toFixed(1)};
 
 const Rockets = ({currentRockets}) => {
   const data = currentRockets;
-  // const inputRef = useRef(null);
   // search logic.
   const [name, setName] = useState('');
   const [rocket, setRocket] = useState('');
@@ -32,27 +31,25 @@ const Rockets = ({currentRockets}) => {
   };
 
   return (
-    <div className=''> 
-      <div className='container flex'>
+    <div> 
+      <div className='flex'>
         <div className='text-left w-full my-2'>
         <h1 className='ml-24 uppercase pb-3 bold text-2xl'>Rockets </h1>
       </div>
-      <div className="relative w-1/2">
-        <form className="flex items-center">   
-          <label htmlFor="voice-search" className="sr-only">Search</label>
-          <div className="relative w-full">
-              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                  <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
-              </div>
-              
-              <input type="search" placeholder="Search" value={name} onChange={filter}
-              className="bg-gray-50 border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-3/4 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-              <button type="button" className="flex absolute inset-y-0 right-0 items-center pr-3">
-                  
-              </button>
-          </div>    
-        </form>
-      </div>
+        <div className="relative w-1/2">
+          <form className="flex items-center">   
+            <label htmlFor="voice-search" className="sr-only">Search</label>
+            <div className="relative w-full">
+                <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                </div>
+                <input type="text"  value={name} onChange={filter}
+                className="bg-gray-50 border rounded-md border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-3/4 pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required="" />
+                <button type="button" className="flex absolute inset-y-0 right-0 items-center pr-3">
+                </button>
+            </div>    
+          </form>
+        </div>
       </div>
       <div>
       <div className="container">
@@ -152,6 +149,5 @@ const RocketsComponent = () => {
     </>
   );
 };
-
 
 export default RocketsComponent

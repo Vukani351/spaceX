68c517e6363b692828a5897a761b8e10f5e5ea34
vukani <gcabashevukani3@gmail.com>
2022-10-06 17:10:29 -1200

    aligning the image for about - had to remove the container

--- src/About.js
+++ src/About.js
@@ -15,12 +15,12 @@ function About (){
       <div className='flex justify-between'>
         <div className="relative"></div>
       </div>
-      <div className= "grid container">
+      <div className= "grid">
         
       <div className='w-full' >
               
           {/* remove the border n shadow & index after setting up */}
-          <div className="mx-2 grid rounded ">
+          <div className="mx-2 grid rounded">
               {/* will add onerror that works correctly after all other things work */}
               {/* <img src={rocket.flickr_images[0]} onError={({ currentTarget }) => { currentTarget.src="https://farm5.staticflickr.com/4599/38583829295_581f34dd84_b.jpg";}} className="w-full max-w-sm rounded-lg overflow-hidden shadow-xl" alt='rocks - spaceX'/> */}
               <img src='./about_img.png'  className="w-full rounded-lg shadow-xl" alt='imag'/>

54007fa15ba7057b138e3e6888af333fb41f04f7
nash4253@gmail.com <gcabashevukani3@gmail.com>
2023-04-02 23:37:06 -1200

    aninmation for search

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,7 @@ import ShipsComponent from './Ships';
 import RocketsComponent from './Rockets';
 
 const Home = () => {
-  
-  // get the kg of that specific mass and round it to nearest integer.
-  function getKg(mass){ return (mass*9.80).toFixed(1)};
-                                                                                                                    
+                                                                                                                      
   return (
     <div className='w-full' >
       <div className='mx-8 grid '> 
